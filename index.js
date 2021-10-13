abfa2ae3546199dd5f1901ccc4fc6e9fb5fc3cd0
Luis Escobar <lae311@stern.nyu.edu>
2021-10-13 12:59:34 -0400

    commit tree 128a1fc06003f7c6c332a29bb6015826448039ed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(2,5);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier;
+    };
+}
+
+function fareDoubler(fare) {
+    const newFare = fare * 2;
+    return newFare;
+}
+
+function fareTripler(fare) {
+    const newFare = fare * 3;
+    return newFare;
+}
+
+function selectDifferentDrivers(arrayofDrivers, func) {
+    return func (arrayofDrivers)
+}
